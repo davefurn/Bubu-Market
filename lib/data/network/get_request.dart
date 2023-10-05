7905b48a9e86477f385801d79d1267b2fdd5461f
OKOH, david <davidokoh2000@gmail.com>
2023-10-05 13:04:14 +0100

    endpoints implementation begins

---
+++ lib/data/network/get_request.dart
@@ -0,0 +1,5 @@
+import 'package:bubu_market/data/network/api_client.dart';
+
+class GetRequest {
+  static final NetworkService network = NetworkService();
+}
---
+++ lib/data/network/post_request.dart
@@ -0,0 +1,5 @@
+import 'package:bubu_market/data/network/api_client.dart';
+
+class PostRequest {
+  static final NetworkService network = NetworkService();
+}
